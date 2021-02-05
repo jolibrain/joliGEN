233634bb4dcc0d8413464939d36925b2efde5992
pnsuau <pnsuau@wanadoo.fr>
2021-02-05 15:16:48 +0100

    feat: nb of images used for FID computation can be chosen

--- options/train_options.py
+++ options/train_options.py
@@ -36,6 +36,8 @@ class TrainOptions(BaseOptions):
         parser.add_argument('--pool_size', type=int, default=50, help='the size of image buffer that stores previously generated images')
         parser.add_argument('--lr_policy', type=str, default='linear', help='learning rate policy. [linear | step | plateau | cosine]')
         parser.add_argument('--lr_decay_iters', type=int, default=50, help='multiply by a gamma every lr_decay_iters iterations')
+        parser.add_argument('--nb_img_max_fid', type=int, default=float("inf"), help='Maximum number of samples allowed per dataset to compute fid. If the dataset directory contains more than nb_img_max_fid, only a subset is used.')
+        
 
         self.isTrain = True
         return parser
