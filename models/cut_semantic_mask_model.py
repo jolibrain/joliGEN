6245fe77490f80823986bdd5659c7b088bddc65c
Emmanuel Benazera <emmanuel.benazera@jolibrain.com>
2021-03-01 16:44:40 +0100

    fix: in place gradient error with f_s

--- models/cut_semantic_mask_model.py
+++ models/cut_semantic_mask_model.py
@@ -252,7 +252,7 @@ class CUTSemanticMaskModel(BaseModel):
             loss_NCE_both = (self.loss_NCE + self.loss_NCE_Y) * 0.5
         else:
             loss_NCE_both = self.loss_NCE
-        self.loss_sem = self.criterionf_s(self.pfB, self.input_A_label)
+        self.loss_sem = self.criterionf_s(self.pfB.detach(), self.input_A_label)
         if self.loss_f_s.detach().item() > 1.0:
             self.loss_sem = 0 * self.loss_sem
         self.loss_G = self.loss_G_GAN + loss_NCE_both + self.loss_sem
