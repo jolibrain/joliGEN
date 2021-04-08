4b67c05eba52f22fe606b8ab8623e4c059587786
Emmanuel Benazera <emmanuel.benazera@jolibrain.com>
2021-04-08 16:02:58 +0200

    fix: image generation script with torch model

--- scripts/gen_single_image.py
+++ scripts/gen_single_image.py
@@ -16,6 +16,7 @@ parser.add_argument('--model-type',default='mobile_resnet_9blocks',help='model t
 parser.add_argument('--img-size',default=256,type=int,help='square image size')
 parser.add_argument('--img-in',help='image to transform',required=True)
 parser.add_argument('--img-out',help='transformed image',required=True)
+parser.add_argument('--cpu',action='store_true',help='whether to use CPU')
 args = parser.parse_args()
 
 # loading model
@@ -31,6 +32,8 @@ model = networks.define_G(input_nc,output_nc,ngf,args.model_type,'instance',use_
                           img_size_dec=args.img_size)
 model.eval()
 model.load_state_dict(torch.load(args.model_in_file))
+if not args.cpu:
+    model = model.cuda()
 
 # reading image
 img = cv2.imread(args.img_in)
@@ -40,12 +43,15 @@ img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 tranlist = [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
 tran = transforms.Compose(tranlist)
 img_tensor = tran(img)
+if not args.cpu:
+    img_tensor = img_tensor.cuda()
 
 # run through model
 out_tensor = model(img_tensor.unsqueeze(0))[0].detach()
 
 # post-processing
-out_img = out_tensor.data[0].cpu().float().numpy()
+out_img = out_tensor.data.cpu().float().numpy()
+print(out_img.shape)
 out_img = (np.transpose(out_img, (1, 2, 0)) + 1) / 2.0 * 255.0
 #print(out_img)
 out_img = cv2.cvtColor(out_img, cv2.COLOR_RGB2BGR)
