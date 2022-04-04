b630a29454968e6d5e2a7e6c54347e2915a4f8ff
pnsuau <pierre-nicolas.suau@jolibrain.com>
2022-04-04 12:12:11 +0000

    doc: fix and update README

--- scripts/train_cut_semantic.sh
+++ scripts/train_cut_semantic.sh
@@ -24,17 +24,14 @@ dataset_mode='unaligned_labeled'
 ######################
 # loss weight params #
 ######################
-lambda_A=50
-lambda_B=50
-lambda_identity=0.5
 lr=0.0002
-d_lr=0.0002
+d_lr=0.0001
 batch=2
 
 ########
 # Model #
 ########
-model='cycle_gan_semantic'
+model='cut'
 netG='mobile_resnet_attn'
 
 ################
@@ -59,7 +56,6 @@ python3 "${current_dir}/../train.py"\
 	--dataroot "${src}" --checkpoints_dir "${checkpoints_dir}" --name $name\
 	--output_display_env $display_env  --output_display_freq ${display_freq} --output_print_freq ${print_freq}\
 	--gpu ${gpu}\
-	--alg_cyclegan_lambda_A ${lambda_A} --alg_cyclegan_lambda_B ${lambda_B} --alg_cyclegan_lambda_identity ${lambda_identity}\
 	--G_lr ${lr} --D_lr ${d_lr} --train_batch_size ${batch}\
 	--data_crop_size ${crop} --data_load_size ${load} --data_dataset_mode ${dataset_mode}\
 	--model_input_nc ${input_nc} --model_output_nc ${output_nc}\
