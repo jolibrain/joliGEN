a66bad5d85d3cef712011e129f46b4d32fe3d553
Emmanuel Benazera <emmanuel.benazera@jolibrain.com>
2022-06-28 14:49:28 +0200

    chore: gen_single_image read config from JSON

--- scripts/gen_single_image.py
+++ scripts/gen_single_image.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import json
 
 sys.path.append("../")
 from models import networks
@@ -20,71 +21,43 @@ def get_z_random(batch_size=1, nz=8, random_type="gauss"):
     return z.detach()
 
 
+##TODO
+def load_model(modelpath, model_in_file, device):
+    train_json_path = modelpath + "/train_config.json"
+    with open(train_json_path, "r") as jsonf:
+        train_json = json.load(jsonf)
+    opt = TrainOptions().parse_json(train_json)
+    if opt.train_mm_nz > 0:
+        opt.model_input_nc += opt.train_mm_nz
+    opt.jg_dir = "../"
+
+    model = networks.define_G(**vars(opt))
+    model.eval()
+    model.load_state_dict(torch.load(modelpath + "/" + model_in_file))
+
+    model = model.to(device)
+    return model, opt
+
+
 parser = argparse.ArgumentParser()
 parser.add_argument(
     "--model-in-file", help="file path to generator model (.pth file)", required=True
 )
-parser.add_argument(
-    "--model-type",
-    default="mobile_resnet_9blocks",
-    help="model type, e.g. mobile_resnet_9blocks",
-)
-parser.add_argument(
-    "--model-config",
-    help="optional model configuration, e.g /path/to/segformer_config_b0.py",
-)
-parser.add_argument(
-    "--padding-type",
-    type=str,
-    help="whether to use padding, zeros or reflect",
-    default="reflect",
-)
+
 parser.add_argument("--img-size", default=256, type=int, help="square image size")
 parser.add_argument("--img-in", help="image to transform", required=True)
 parser.add_argument("--img-out", help="transformed image", required=True)
-parser.add_argument("--bw", action="store_true", help="whether input/output is bw")
 parser.add_argument("--cpu", action="store_true", help="whether to use CPU")
 parser.add_argument("--gpuid", type=int, default=0, help="which GPU to use")
-parser.add_argument(
-    "--nz",
-    type=int,
-    default=-1,
-    help="size of the latent for multimodal models, e.g. 8",
-)
 args = parser.parse_args()
 
-if args.bw:
-    input_nc = output_nc = 1
-else:
-    input_nc = output_nc = 3
-if args.nz > 0:
-    input_nc += args.nz
-
 # loading model
-opt = TrainOptions().parse_json({})
-opt.data_crop_size = args.img_size
-opt.data_load_size = args.img_size
-opt.G_attn_nb_mask_attn = 10
-opt.G_attn_nb_mask_input = 1
-opt.G_netG = args.model_type
-opt.G_padding_type = args.padding_type
-opt.model_input_nc = input_nc
-opt.model_output_nc = output_nc
-if args.nz > 0:
-    opt.model_multimodal = True
-if "segformer" in args.model_type:
-    opt.G_config_segformer = (
-        args.model_config
-    )  # e.g. '/path/to/models/configs/segformer/segformer_config_b0.py'
-opt.jg_dir = os.path.join("/".join(__file__.split("/")[:-2]))
-model = networks.define_G(**vars(opt))
-
-model.eval()
-model.load_state_dict(torch.load(args.model_in_file))
+modelpath = args.model_in_file.replace(os.path.basename(args.model_in_file), "")
+print("modelpath=", modelpath)
 
 if not args.cpu:
     device = torch.device("cuda:" + str(args.gpuid))
-    model = model.to(device)
+model, opt = load_model(modelpath, os.path.basename(args.model_in_file), device)
 
 # reading image
 img = cv2.imread(args.img_in)
@@ -100,8 +73,8 @@ img_tensor = tran(img)
 if not args.cpu:
     img_tensor = img_tensor.to(device)
 
-if args.nz > 0:
-    z_random = get_z_random(batch_size=1, nz=args.nz)
+if opt.train_mm_nz > 0:
+    z_random = get_z_random(batch_size=1, nz=opt.train_mm_nz)
     z_random = z_random.to(device)
     # print('z_random shape=', self.z_random.shape)
     z_real = z_random.view(z_random.size(0), z_random.size(1), 1, 1).expand(
