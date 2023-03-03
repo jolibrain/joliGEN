bb012b698addabdd57772dd35305d97498bdbd2c
killian31 <killian.steunou@gmail.com>
2023-03-03 15:21:23 +0100

    feat: canny edge detection

--- util/mask_generation.py
+++ util/mask_generation.py
@@ -41,3 +41,21 @@ def fill_img_with_edges(img, mask):
     mask = torch.clamp(mask, 0, 1)
 
     return mask * edges + (1 - mask) * img
+
+def fill_img_with_canny(img, mask):
+    
+    img_orig = img.clone()
+    mask_2D = mask.cpu()[0,:,:][0]    # Convert mask to a 2D array
+    coords = np.column_stack(np.where(mask_2D > 0))   # Get the coordinates of the white pixels in the mask
+    x_0, y_0, w, h = cv2.boundingRect(coords.astype(np.int))
+    ## TODO check if [:, :, w, h] or invert w and h ?
+    to_sketch = img_orig[:, :, x_0:x_0 + w, y_0:y_0 + h]
+    
+    to_sketch = np.transpose(to_sketch.squeeze().cpu().numpy(), (1, 2, 0))
+    edges = cv2.Canny((to_sketch*255).astype(np.uint8), 250, 500)
+    # edges = np.transpose(edges, (2, 0, 1))
+    edges = torch.from_numpy(edges).unsqueeze(0).unsqueeze(0)
+    inverted_edges = torch.abs(edges - 1)
+    img_orig[:, :, x_0:x_0 + w, y_0:y_0 + h] = inverted_edges
+    
+    return img_orig
