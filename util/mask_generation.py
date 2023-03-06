12eddcd480438718f92e4608c30e81caa01b55ca
killian31 <killian.steunou@gmail.com>
2023-03-06 15:40:30 +0100

    fix: not updated model weights

--- util/mask_generation.py
+++ util/mask_generation.py
@@ -1,9 +1,10 @@
-import torch
-from torchvision.transforms import Grayscale
+import os
+import urllib.request
+
 import cv2
 import numpy as np
-import urllib.request
-import os
+import torch
+from torchvision.transforms import Grayscale
 
 
 def fill_img_with_sketch(img, mask):
@@ -20,7 +21,6 @@ def fill_img_with_sketch(img, mask):
 
 
 def fill_img_with_edges(img, mask):
-
     device = img.device
 
     edges_list = []
@@ -44,22 +44,25 @@ def fill_img_with_edges(img, mask):
 
     return mask * edges + (1 - mask) * img
 
-def fill_img_with_canny(img, mask):
 
+def fill_img_with_canny(img, mask):
     img_orig = img.clone()
-    mask_2D = mask.cpu()[0,:,:][0]    # Convert mask to a 2D array
-    coords = np.column_stack(np.where(mask_2D > 0))   # Get the coordinates of the white pixels in the mask
+    mask_2D = mask.cpu()[0, :, :][0]  # Convert mask to a 2D array
+    coords = np.column_stack(
+        np.where(mask_2D > 0)
+    )  # Get the coordinates of the white pixels in the mask
     x_0, y_0, w, h = cv2.boundingRect(coords.astype(np.int))
     ## TODO check if [:, :, w, h] or invert w and h ?
-    to_sketch = img_orig[:, :, x_0:x_0 + w, y_0:y_0 + h]
+    to_sketch = img_orig[:, :, x_0 : x_0 + w, y_0 : y_0 + h]
 
     to_sketch = np.transpose(to_sketch.squeeze().cpu().numpy(), (1, 2, 0))
-    edges = cv2.Canny((to_sketch*255).astype(np.uint8), 250, 500)
+    edges = cv2.Canny((to_sketch * 255).astype(np.uint8), 250, 500)
     # edges = np.transpose(edges, (2, 0, 1))
     edges = torch.from_numpy(edges).unsqueeze(0).unsqueeze(0)
-    img_orig[:, :, x_0:x_0 + w, y_0:y_0 + h] = edges
+    img_orig[:, :, x_0 : x_0 + w, y_0 : y_0 + h] = edges
 
     return img_orig
+
 
 def fill_img_with_hed(img, mask):
     """
@@ -67,10 +70,12 @@ def fill_img_with_hed(img, mask):
     """
 
     img_orig = img.clone()
-    mask_2D = mask.cpu()[0,:,:][0]    # Convert mask to a 2D array
-    coords = np.column_stack(np.where(mask_2D > 0))   # Get the coordinates of the white pixels in the mask
+    mask_2D = mask.cpu()[0, :, :][0]  # Convert mask to a 2D array
+    coords = np.column_stack(
+        np.where(mask_2D > 0)
+    )  # Get the coordinates of the white pixels in the mask
     x_0, y_0, w, h = cv2.boundingRect(coords.astype(np.int))
-    to_sketch = img_orig[:, :, x_0:x_0 + w, y_0:y_0 + h]
+    to_sketch = img_orig[:, :, x_0 : x_0 + w, y_0 : y_0 + h]
 
     to_sketch = np.transpose(to_sketch.squeeze().cpu().numpy(), (1, 2, 0))
 
@@ -78,14 +83,16 @@ def fill_img_with_hed(img, mask):
 
     (H, W) = img_to_sketch.shape[:2]
 
-    blob = cv2.dnn.blobFromImage(img_to_sketch, scalefactor=1.0, size=(W, H), swapRB=False, crop=False)
+    blob = cv2.dnn.blobFromImage(
+        img_to_sketch, scalefactor=1.0, size=(W, H), swapRB=False, crop=False
+    )
 
-    proto_url = 'https://raw.githubusercontent.com/richzhang/colorization/caffe/models/colorization_deploy_v2.prototxt'
-    weights_url = 'http://eecs.berkeley.edu/~rich.zhang/projects/2016_colorization/files/demo_v2/colorization_release_v2.caffemodel'
-    
-    p_filename = 'deploy.protoxt'
-    w_filename = 'pretrained_hed.caffemodel'
-    folder = '../models/modules/caffe'
+    proto_url = "https://raw.githubusercontent.com/richzhang/colorization/caffe/models/colorization_deploy_v2.prototxt"
+    weights_url = "http://eecs.berkeley.edu/~rich.zhang/projects/2016_colorization/files/demo_v2/colorization_release_v2.caffemodel"
+
+    p_filename = "deploy.protoxt"
+    w_filename = "pretrained_hed.caffemodel"
+    folder = "../models/modules/caffe"
 
     p_path = os.path.join(folder, p_filename)
     w_path = os.path.join(folder, w_filename)
@@ -107,6 +114,6 @@ def fill_img_with_hed(img, mask):
 
     hed = torch.from_numpy(hed).unsqueeze(0).unsqueeze(0)
 
-    img_orig[:, :, x_0:x_0 + w, y_0:y_0 + h] = hed
+    img_orig[:, :, x_0 : x_0 + w, y_0 : y_0 + h] = hed
 
     return img_orig
