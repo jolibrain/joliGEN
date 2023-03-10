eba7ab8212000864f2b402fe7172ff0f60fb4dc6
Emmanuel Benazera <emmanuel.benazera@jolibrain.com>
2023-03-10 15:53:12 +0000

    fix: use only mask from selected bbox at inference time

--- scripts/gen_single_image_diffusion.py
+++ scripts/gen_single_image_diffusion.py
@@ -142,7 +142,8 @@ def generate(
             hc_width = int(crop_width / 2)
             hc_height = int(crop_height / 2)
             # select one bbox and crop around it
-            bbox_orig = random.choice(bboxes)
+            bbox_idx = random.choice(range(len(bboxes)))
+            bbox_orig = bboxes[bbox_idx]
             if bbox_width_factor > 0.0:
                 bbox_orig[0] -= max(0, int(bbox_width_factor * bbox_orig[0]))
                 bbox_orig[2] += max(0, int(bbox_width_factor * bbox_orig[2]))
@@ -193,6 +194,7 @@ def generate(
             load_size=opt.data_online_creation_load_size_A,
             get_crop_coordinates=True,
             crop_center=True,
+            bbox_ref_id=bbox_idx,
         )
 
         img, mask = crop_image(
@@ -208,6 +210,7 @@ def generate(
             load_size=opt.data_online_creation_load_size_A,
             crop_coordinates=crop_coordinates,
             crop_center=True,
+            bbox_ref_id=bbox_idx,
         )
 
         x_crop, y_crop, crop_size = crop_coordinates
