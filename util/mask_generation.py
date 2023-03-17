7851653d5c4c1f02e487bf24f30e33d24c44277c
killian31 <killian.steunou@gmail.com>
2023-03-17 11:15:31 +0100

    fix: canny for training

--- util/mask_generation.py
+++ util/mask_generation.py
@@ -12,6 +12,7 @@ from torchvision.transforms import Grayscale
 
 from models.mbv2_mlsd_large import MobileV2_MLSD_Large
 from models.modules.utils import download_midas_weight, predict_depth
+from util.util import tensor2im
 
 sys.path.append("./../")
 
@@ -370,23 +371,41 @@ def fill_img_with_edges(img, mask):
 
 
 def fill_img_with_canny(img, mask, low_threshold=150, high_threshold=200):
+    device = img.device
+    edges_list = []
+    for cur_img in img:
+        cur_img = (
+            (torch.einsum("chw->hwc", cur_img).cpu().numpy() + 1) * 255 / 2
+        ).astype(np.uint8)
+        edges = cv2.Canny(cur_img, low_threshold, high_threshold)
+        edges = (
+            (((torch.tensor(edges, device=device) / 255) * 2) - 1)
+            .unsqueeze(0)
+            .unsqueeze(0)
+        )
+        edges_list.append(edges)
+    edges = torch.cat(edges_list, dim=0)
+    mask = torch.clamp(mask, 0, 1)
+
+    return mask * edges + (1 - mask) * img
+    """
     batch_output = torch.zeros_like(img)
     for i in range(img.shape[0]):
         img_orig = img[i].clone()
         mask_2D = mask.cpu()[i].squeeze(0)  # Convert mask to a 2D array
         coords = np.column_stack(
-            np.where(mask_2D > 0.9)
+            np.where(mask_2D > 0)
         )  # Get the coordinates of the white pixels in the mask
         x_0, y_0, w, h = cv2.boundingRect(coords.astype(int))
 
         to_sketch = img_orig[:, x_0 : x_0 + w, y_0 : y_0 + h]
+
+        to_sketch = (
+            (torch.einsum("chw->hwc", to_sketch).cpu().numpy() + 1) * 255 / 2
+        ).astype(np.uint8)
         print(to_sketch.shape)
-        to_sketch = np.transpose(to_sketch.squeeze().cpu().numpy(), (1, 2, 0))
-        print(to_sketch)
-        cv2.imwrite("test.png", to_sketch * 255)
-        edges = cv2.Canny(
-            (to_sketch * 255).astype(np.uint8), low_threshold, high_threshold
-        )
+        cv2.imwrite("test.png", to_sketch)
+        edges = cv2.Canny(to_sketch, low_threshold, high_threshold)
         if list(mask_2D[mask_2D > 0]) == []:
             batch_output[i] = img_orig
         else:
@@ -394,24 +413,6 @@ def fill_img_with_canny(img, mask, low_threshold=150, high_threshold=200):
             img_orig[:, x_0 : x_0 + w, y_0 : y_0 + h] = edges
             batch_output[i] = img_orig
     return batch_output
-    """
-    img_orig = img.clone()
-    mask_2D = mask.cpu()[0, :, :][0]  # Convert mask to a 2D array
-    coords = np.column_stack(
-        np.where(mask_2D > 0.9)
-    )  # Get the coordinates of the white pixels in the mask
-    x_0, y_0, w, h = cv2.boundingRect(coords.astype(int))
-
-    to_sketch = img_orig[:, :, x_0 : x_0 + w, y_0 : y_0 + h]
-
-    to_sketch = np.transpose(to_sketch.squeeze().cpu().numpy(), (1, 2, 0))
-    edges = cv2.Canny((to_sketch * 255).astype(np.uint8), low_threshold, high_threshold)
-
-    # edges = np.transpose(edges, (2, 0, 1))
-    edges = torch.from_numpy(edges).unsqueeze(0).unsqueeze(0)
-    img_orig[:, :, x_0 : x_0 + w, y_0 : y_0 + h] = edges
-
-    return img_orig
     """
 
 
