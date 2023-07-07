1ae7c05fb23252b98eee33673b53f1e220ac68d9
Alexandre Girard <git@alexgirard.com>
2023-07-07 13:09:09 +0000

    refactor: use launch_predict method

--- scripts/gen_single_image.py
+++ scripts/gen_single_image.py
@@ -44,62 +44,66 @@ def load_model(modelpath, model_in_file, cpu, gpuid):
     model = model.to(device)
     return model, opt, device
 
+def launch_predict(args):
+    # loading model
+    modelpath = args.model_in_file.replace(os.path.basename(args.model_in_file), "")
+    print("modelpath=", modelpath)
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "--model-in-file", help="file path to generator model (.pth file)", required=True
-)
-
-parser.add_argument("--img-in", help="image to transform", required=True)
-parser.add_argument("--img-out", help="transformed image", required=True)
-parser.add_argument("--cpu", action="store_true", help="whether to use CPU")
-parser.add_argument("--gpuid", type=int, default=0, help="which GPU to use")
-args = parser.parse_args()
-
-# loading model
-modelpath = args.model_in_file.replace(os.path.basename(args.model_in_file), "")
-print("modelpath=", modelpath)
-
-model, opt, device = load_model(
-    modelpath, os.path.basename(args.model_in_file), args.cpu, args.gpuid
-)
-
-# reading image
-img = cv2.imread(args.img_in)
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-# preprocessing
-tranlist = [
-    transforms.ToTensor(),
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-]
-tran = transforms.Compose(tranlist)
-img_tensor = tran(img)
-if not args.cpu:
-    img_tensor = img_tensor.to(device)
-
-if opt.model_multimodal:
-    z_random = get_z_random(batch_size=1, nz=opt.train_mm_nz)
-    z_random = z_random.to(device)
-    # print('z_random shape=', self.z_random.shape)
-    z_real = z_random.view(z_random.size(0), z_random.size(1), 1, 1).expand(
-        z_random.size(0),
-        z_random.size(1),
-        img_tensor.size(1),
-        img_tensor.size(2),
+    model, opt, device = load_model(
+        modelpath, os.path.basename(args.model_in_file), args.cpu, args.gpuid
     )
-    img_tensor = torch.cat([img_tensor.unsqueeze(0), z_real], 1)
-else:
-    img_tensor = img_tensor.unsqueeze(0)
 
-# run through model
-out_tensor = model(img_tensor)[0].detach()
+    # reading image
+    img = cv2.imread(args.img_in)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-# post-processing
-out_img = out_tensor.data.cpu().float().numpy()
-print(out_img.shape)
-out_img = (np.transpose(out_img, (1, 2, 0)) + 1) / 2.0 * 255.0
-# print(out_img)
-out_img = cv2.cvtColor(out_img, cv2.COLOR_RGB2BGR)
-cv2.imwrite(args.img_out, out_img)
-print("Successfully generated image ", args.img_out)
+    # preprocessing
+    tranlist = [
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+    ]
+    tran = transforms.Compose(tranlist)
+    img_tensor = tran(img)
+    if not args.cpu:
+        img_tensor = img_tensor.to(device)
+
+    if opt.model_multimodal:
+        z_random = get_z_random(batch_size=1, nz=opt.train_mm_nz)
+        z_random = z_random.to(device)
+        # print('z_random shape=', self.z_random.shape)
+        z_real = z_random.view(z_random.size(0), z_random.size(1), 1, 1).expand(
+            z_random.size(0),
+            z_random.size(1),
+            img_tensor.size(1),
+            img_tensor.size(2),
+        )
+        img_tensor = torch.cat([img_tensor.unsqueeze(0), z_real], 1)
+    else:
+        img_tensor = img_tensor.unsqueeze(0)
+
+    # run through model
+    out_tensor = model(img_tensor)[0].detach()
+
+    # post-processing
+    out_img = out_tensor.data.cpu().float().numpy()
+    print(out_img.shape)
+    out_img = (np.transpose(out_img, (1, 2, 0)) + 1) / 2.0 * 255.0
+    # print(out_img)
+    out_img = cv2.cvtColor(out_img, cv2.COLOR_RGB2BGR)
+    cv2.imwrite(args.img_out, out_img)
+    print("Successfully generated image ", args.img_out)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--model-in-file", help="file path to generator model (.pth file)", required=True
+    )
+
+    parser.add_argument("--img-in", help="image to transform", required=True)
+    parser.add_argument("--img-out", help="transformed image", required=True)
+    parser.add_argument("--cpu", action="store_true", help="whether to use CPU")
+    parser.add_argument("--gpuid", type=int, default=0, help="which GPU to use")
+    args = parser.parse_args()
+
+    launch_predict(args)
