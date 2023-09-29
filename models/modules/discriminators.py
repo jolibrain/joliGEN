232ae288c4633d1250b701b4821fe575bc3c632d
julie <wr0124@gmail.com>
2023-09-29 17:47:33 +0200

    feat: D_ngf and D_num_downs

--- models/modules/discriminators.py
+++ models/modules/discriminators.py
@@ -161,7 +161,7 @@ class UnetDiscriminator(nn.Module):
         input_nc,
         output_nc,
         D_num_downs,
-        ngf=64,
+        D_ngf=64,
         norm_layer=nn.BatchNorm2d,
         use_dropout=False,
     ):
@@ -171,7 +171,7 @@ class UnetDiscriminator(nn.Module):
             output_nc (int) -- the number of channels in output images
             D_num_downs (int) -- the number of downsamplings in UNet. For example, # if |num_downs| == 7,
                                 image of size 128x128 will become of size 1x1 # at the bottleneck
-            ngf (int)       -- the number of filters in the last conv layer, here  ngf=64, so inner_nc=64*8=512
+            D_ngf (int)       -- the number of filters in the last conv layer, here  ngf=64, so inner_nc=64*8=512
             norm_layer      -- normalization layer
 
         We construct the U-Net from the innermost layer to the outermost layer.
@@ -181,18 +181,18 @@ class UnetDiscriminator(nn.Module):
         # construct unet structure
         # add the innermost layer
         unet_block = UnetSkipConnectionBlock(
-            ngf * 8,
-            ngf * 8,
+            D_ngf * 8,
+            D_ngf * 8,
             input_nc=None,
             submodule=None,
             norm_layer=norm_layer,
             innermost=True,
         )
         # add intermediate layers with ngf * 8 filters
-        for i in range(num_downs - 5):  # add intermediate layers with ngf * 8 filters
+        for i in range(D_num_downs - 5):  # add intermediate layers with ngf * 8 filters
             unet_block = UnetSkipConnectionBlock(
-                ngf * 8,
-                ngf * 8,
+                D_ngf * 8,
+                D_ngf * 8,
                 input_nc=None,
                 submodule=unet_block,
                 norm_layer=norm_layer,
@@ -200,19 +200,27 @@ class UnetDiscriminator(nn.Module):
             )
         # gradually reduce the number of filters from ngf * 8 to ngf
         unet_block = UnetSkipConnectionBlock(
-            ngf * 4, ngf * 8, input_nc=None, submodule=unet_block, norm_layer=norm_layer
+            D_ngf * 4,
+            D_ngf * 8,
+            input_nc=None,
+            submodule=unet_block,
+            norm_layer=norm_layer,
         )
         unet_block = UnetSkipConnectionBlock(
-            ngf * 2, ngf * 4, input_nc=None, submodule=unet_block, norm_layer=norm_layer
+            D_ngf * 2,
+            D_ngf * 4,
+            input_nc=None,
+            submodule=unet_block,
+            norm_layer=norm_layer,
         )
         unet_block = UnetSkipConnectionBlock(
-            ngf, ngf * 2, input_nc=None, submodule=unet_block, norm_layer=norm_layer
+            D_ngf, D_ngf * 2, input_nc=None, submodule=unet_block, norm_layer=norm_layer
         )
 
         # add the outermost layer
         self.model = UnetSkipConnectionBlock(
             output_nc,
-            ngf,
+            D_ngf,
             input_nc=input_nc,
             submodule=unet_block,
             outermost=True,
