17c7089c17cc332b1bba28782a89e601815344a8
julie <wr0124@gmail.com>
2023-09-29 14:46:52 +0200

    feat: add num_donws and D_ngf=64

--- models/gan_networks.py
+++ models/gan_networks.py
@@ -446,7 +446,7 @@ def define_D(
                 model_input_nc,
                 model_output_nc,
                 num_downs=7,
-                D_ngf=64,
+                D_ngf=64, # the final conv has D_ngf*8=512 filter
                 norm_layer=norm_layer,
                 use_dropout=D_dropout,
             )
