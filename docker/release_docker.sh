f1c56de0e2f8468f0ae49ae75714271e2a28a7fa
Emmanuel Benazera <emmanuel.benazera@jolibrain.com>
2023-10-09 10:33:00 +0200

    fix: docker release script

--- docker/release_docker.sh
+++ docker/release_docker.sh
@@ -5,3 +5,11 @@ echo $gt
 command="docker build -t docker.jolibrain.com/joligen_build:$gt -f docker/Dockerfile.build ."
 echo $command
 eval $command
+
+command="docker build -t docker.jolibrain.com/joligen_server:$gt --build-arg GTAG=$gt -f docker/Dockerfile.server ."
+echo $command
+eval $command
+
+command="docker push docker.jolibrain.com/joligen_server:$gt"
+echo $command
+eval $command
