484456a5f4933147aa95f1a22c746bd01cc2cb7a
Emmanuel Benazera <emmanuel.benazera@jolibrain.com>
2024-11-21 19:14:15 +0000

    chore: deactivate img2img_turbo tests

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -87,14 +87,14 @@ fi
 
 
 ###### test img2img_turbo
-echo "Running test img2img_turbo"
+# echo "Running test img2img_turbo"
 
-python3 -m pytest --rootdir ${current_dir} -p no:cacheprovider -s "${current_dir}/../tests/test_run_img2img_turbo.py" --dataroot "$TARGET_NOSEM_DIR"
-OUT=$?
+# python3 -m pytest --rootdir ${current_dir} -p no:cacheprovider -s "${current_dir}/../tests/test_run_img2img_turbo.py" --dataroot "$TARGET_NOSEM_DIR"
+# OUT=$?
 
-if [ $OUT != 0 ]; then
-    exit 1
-fi
+# if [ $OUT != 0 ]; then
+#     exit 1
+# fi
 
 ####### mask semantics test
 echo "Running mask semantics training tests"
