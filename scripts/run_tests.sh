34e301cdd5cdc3fa609873c766e94b723b68b1c3
julie wang <julie.wang@jolibrain.com>
2026-01-08 10:26:49 +0100

    feat(ml):unit test

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -161,6 +161,16 @@ if [ $OUT != 0 ]; then
     exit 1
 fi
 
+###### b2b model process test
+echo "Running b2b model process test"
+python3 -m pytest --rootdir ${current_dir} -p no:cacheprovider -s "${current_dir}/../tests/test_run_b2b.py" --dataroot "$TARGET_MASK_SEM_DIR"
+OUT=$?
+
+if [ $OUT != 0 ]; then
+    exit 1
+fi
+
+
 ###### GAN+supervised super-resolution process test
 echo "Running GAN+supervised super-resolution process test"
 python3 -m pytest --rootdir ${current_dir} -p no:cacheprovider -s "${current_dir}/../tests/test_run_sr_gan.py" --dataroot "$TARGET_MASK_SEM_DIR"
