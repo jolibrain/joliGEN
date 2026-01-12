c6bb27e23a205772322b9a961407198631d8c354
julie wang <julie.wang@jolibrain.com>
2026-01-12 11:23:13 +0100

    feat(ml): enlarger loss calculation

--- models/modules/vit/vit.py
+++ models/modules/vit/vit.py
@@ -425,85 +425,6 @@ class JiT(nn.Module):
         return output
 
 
-#
-# def JiT_B_16(**kwargs):
-#    return JiT(
-#        depth=12,
-#        hidden_size=768,
-#        num_heads=12,
-#        bottleneck_dim=128,
-#        in_context_len=32,
-#        in_context_start=4,
-#        patch_size=16,
-#        **kwargs,
-#    )
-#
-#
-# def JiT_B_32(**kwargs):
-#    return JiT(
-#        depth=12,
-#        hidden_size=768,
-#        num_heads=12,
-#        bottleneck_dim=128,
-#        in_context_len=32,
-#        in_context_start=4,
-#        patch_size=32,
-#        **kwargs,
-#    )
-#
-#
-# def JiT_L_16(**kwargs):
-#    return JiT(
-#        depth=24,
-#        hidden_size=1024,
-#        num_heads=16,
-#        bottleneck_dim=128,
-#        in_context_len=32,
-#        in_context_start=8,
-#        patch_size=16,
-#        **kwargs,
-#    )
-#
-#
-# def JiT_L_32(**kwargs):
-#    return JiT(
-#        depth=24,
-#        hidden_size=1024,
-#        num_heads=16,
-#        bottleneck_dim=128,
-#        in_context_len=32,
-#        in_context_start=8,
-#        patch_size=32,
-#        **kwargs,
-#    )
-#
-#
-# def JiT_H_16(**kwargs):
-#    return JiT(
-#        depth=32,
-#        hidden_size=1280,
-#        num_heads=16,
-#        bottleneck_dim=256,
-#        in_context_len=32,
-#        in_context_start=10,
-#        patch_size=16,
-#        **kwargs,
-#    )
-#
-#
-# def JiT_H_32(**kwargs):
-#    return JiT(
-#        depth=32,
-#        hidden_size=1280,
-#        num_heads=16,
-#        bottleneck_dim=256,
-#        in_context_len=32,
-#        in_context_start=10,
-#        patch_size=32,
-#        **kwargs,
-#    )
-#
-
 JiT_VARIANT_CONFIGS = {
     "JiT-B/16": dict(
         depth=12,
@@ -513,6 +434,15 @@ JiT_VARIANT_CONFIGS = {
         in_context_len=32,
         in_context_start=4,
         patch_size=16,
+    ),
+    "JiT-B/8": dict(
+        depth=12,
+        hidden_size=768,
+        num_heads=12,
+        bottleneck_dim=128,
+        in_context_len=32,
+        in_context_start=4,
+        patch_size=8,
     ),
     "JiT-B/32": dict(
         depth=12,
@@ -560,12 +490,3 @@ JiT_VARIANT_CONFIGS = {
         patch_size=32,
     ),
 }
-
-# JiT_models = {
-#    "JiT-B/16": JiT_B_16,
-#    "JiT-B/32": JiT_B_32,
-#    "JiT-L/16": JiT_L_16,
-#    "JiT-L/32": JiT_L_32,
-#    "JiT-H/16": JiT_H_16,
-#    "JiT-H/32": JiT_H_32,
-# }
