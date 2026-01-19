0aa5714c187b86d78af8b409a22634d4fa1a82ab
julie wang <julie.wang@jolibrain.com>
2026-01-19 11:48:30 +0100

    feat(ml): add motion module into vit

--- models/modules/vit/vit_vid.py
+++ models/modules/vit/vit_vid.py
@@ -26,9 +26,8 @@ def modulate(x, shift, scale):
 def scaled_dot_product_attention(query, key, value, dropout_p=0.0) -> torch.Tensor:
     L, S = query.size(-2), key.size(-2)
     scale_factor = 1 / math.sqrt(query.size(-1))
-    attn_bias = torch.zeros(
-        query.size(0), 1, L, S, dtype=query.dtype, device=query.device
-    )
+    attn_bias = torch.zeros(query.size(0), 1, L, S, dtype=query.dtype).cuda()
+
     with torch.cuda.amp.autocast(enabled=False):
         attn_weight = query.float() @ key.float().transpose(-2, -1) * scale_factor
     attn_weight += attn_bias
