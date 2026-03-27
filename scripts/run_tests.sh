da9ff838b168add77410e6a8a99f3dd92d4ef330
Emmanuel Benazera <emmanuel.benazera@jolibrain.com>
2026-03-27 09:18:45 +0000

    chore: adding test for mat
    chore: mat model vid unit test

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -170,6 +170,15 @@ if [ $OUT != 0 ]; then
     exit 1
 fi
 
+###### mat model unit tests
+echo "Running mat model unit tests"
+python3 -m pytest --rootdir ${current_dir} -p no:cacheprovider -s "${current_dir}/../tests/test_mat_model.py"
+OUT=$?
+
+if [ $OUT != 0 ]; then
+    exit 1
+fi
+
 
 ###### GAN+supervised super-resolution process test
 echo "Running GAN+supervised super-resolution process test"
