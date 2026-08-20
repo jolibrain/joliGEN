5d8c54754e492458fbb55d1a5a097b036722b3ad
Emmanuel Benazera <emmanuel.benazera@jolibrain.com>
2026-08-20 12:12:13 +0000

    feat: bbox context augmentation for b2b

--- data/online_creation.py
+++ data/online_creation.py
@@ -1767,9 +1767,9 @@ def fill_mask_with_color(img, mask, colors):
         mask = torch.where(mask == cls, 1.0, 0.0)
         dims = img.shape
 
-        assert (
-            len(color) == dims[-3]
-        ), "fill_mask_with_color: number of channels does not match"
+        assert len(color) == dims[-3], (
+            "fill_mask_with_color: number of channels does not match"
+        )
         color = torch.tensor(color).repeat_interleave(dims[-2] * dims[-1]).reshape(dims)
         img = img * (1 - mask) + color * mask
 
